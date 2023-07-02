935cd780dc747a224ffbf5b1c3dddb90d266d8a3
Rudy Huynh <rudy.huynh286@gmail.com>
2023-07-03 01:03:06 +0700

    setup for production

--- aif-server/src/main.ts
+++ aif-server/src/main.ts
@@ -11,6 +11,10 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public"));
+}
+
 async function main() {
   initGraphQLClient();
 
--- aif-web/src/constants.ts
+++ aif-web/src/constants.ts
@@ -1,1 +1,2 @@
-export const ENDPOINT = "http://localhost:3001";
+export const ENDPOINT =
+  process.env.NODE_ENV === "development" ? "http://localhost:3001" : "";
